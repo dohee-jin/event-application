661d48a37eecc63f02131396c0d70c31bcf5bf50
dohee <doheeeq@gmail.com>
2025-09-02 09:27:39 +0900

    [FE] 액션함수 서버 요청 로직 작성

--- frontend/src/components/EventForm.jsx
+++ frontend/src/components/EventForm.jsx
@@ -1,53 +1,9 @@
 import styles from './EventForm.module.scss';
-import {useRef} from "react";
-import {useNavigate, Form} from "react-router-dom";
+import {Form} from "react-router-dom";
 
 const EventForm = () => {
 
-    const titleRef = useRef();
-    const imageRef = useRef();
-    const dateRef = useRef();
-    const descRef = useRef();
 
-    const navigate = useNavigate();
-
-
-    const handleSubmit = e => {
-        e.preventDefault();
-
-        const $title = titleRef?.current;
-        const $image = imageRef?.current;
-        const $date = dateRef?.current;
-        const $desc = descRef?.current;
-
-        const payload = {
-            title: $title.value,
-            desc: $desc.value,
-            beginDate: $date.value,
-            imageUrl: $image.value
-        }
-
-        console.log(payload);
-
-        // 즉시 실행 함수로 fetch 요청 보냄
-        (async () => {
-            const response = await fetch(`http://localhost:9000/api/events`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(payload)
-            });
-            if(response.ok) {
-                navigate('/events');
-            }
-        })();
-
-        $title.value = '';
-        $desc.value = '';
-        $date.value = '';
-        $image.value = '';
-    }
 
     // route 설정에 있는 action 함수를 트리거하려면 Form 이라는 컴포넌트가 필요하다
     // 필수 속성으로 method 속성을 지정해야 한다.
@@ -64,7 +20,6 @@ const EventForm = () => {
                     type='text'
                     name='title'
                     required
-                    ref={titleRef}
                 />
             </p>
             <p>
@@ -74,7 +29,6 @@ const EventForm = () => {
                     type='url'
                     name='image'
                     required
-                    ref={imageRef}
                 />
             </p>
             <p>
@@ -84,7 +38,6 @@ const EventForm = () => {
                     type='date'
                     name='date'
                     required
-                    ref={dateRef}
                 />
             </p>
             <p>
@@ -94,7 +47,6 @@ const EventForm = () => {
                     name='description'
                     rows='5'
                     required
-                    ref={descRef}
                 />
             </p>
             <div className={styles.actions}>
--- frontend/src/loader/event-actions.js
+++ frontend/src/loader/event-actions.js
@@ -1,3 +1,34 @@
-export const saveAction = () => {
-    console.log('save action!')
+import {redirect} from "react-router-dom";
+
+export const saveAction = async ({ request }) => {
+   // const formData = new FormData(e.target);
+
+    // form에 입력한 값 가져오기
+    // await를 붙여서 비동기적인 기능순서에서 순서를 보장해줌
+    const formData = await request.formData();
+    // 서버로 보낼 payload
+    const payload = {
+        title: formData.get('title'),
+        desc: formData.get('description'),
+        beginDate: formData.get('date'),
+        imageUrl: formData.get('image')
+    };
+    // console.log(payload);
+
+    const response = await fetch('http://localhost:9000/api/events', {
+        method: request.method,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload)
+    })
+
+    if(!response.ok) {
+        throw new Error('이벤트 생성에 실패했습니다!')
+    }
+
+    // 목록페이지로 리다이렉트
+    // action 함수에서만 제공하는 함수
+    return redirect('/events');
+
 };
