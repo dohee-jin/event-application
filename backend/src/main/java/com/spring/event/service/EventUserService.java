ecac0b4db5c34cc23522922dd9dddc718d39dc11
dohee <doheeeq@gmail.com>
2025-09-03 09:29:36 +0900

    [BE] 이메일 중복확인 api

---
+++ backend/src/main/java/com/spring/event/service/EventUserService.java
@@ -0,0 +1,32 @@
+package com.spring.event.service;
+
+import com.spring.event.domain.entity.EventUser;
+import com.spring.event.repository.EventUserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+@Slf4j
+public class EventUserService {
+
+    private final EventUserRepository eventUserRepository;
+
+
+    // 이메일 충복확인 처리
+    public boolean checkEmailDuplicate(String email) {
+       // 임시로 회원가입 진행
+        EventUser eventUser = EventUser.builder()
+                .email("abc@def" + (int) (Math.random() * 99999))
+                .build() ;
+        eventUserRepository.save(eventUser);
+
+        // 중복확인
+        boolean flag = eventUserRepository.existsByEmail(email);
+        log.info("Checking email {} is duplicate: {}", email, flag);
+        return flag;
+    }
+}
