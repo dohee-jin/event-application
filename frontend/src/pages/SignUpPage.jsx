7c82020a95f079f3ad9d1c1581474248b3b90775
dohee <doheeeq@gmail.com>
2025-09-03 10:44:09 +0900

    [FE] 인증관련 컴포넌트 생성

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import SignUpForm from "../components/auth/SignUpForm.jsx";
 
 const SignUpPage = () => {
-    return <h1>SignUpPage!</h1>;
+    return (
+        <SignUpForm />
+    )
 };
 
 export default SignUpPage;
