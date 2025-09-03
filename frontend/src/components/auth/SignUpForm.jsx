50cc56315eca18e6a521304baab177b477e339fa
dohee <doheeeq@gmail.com>
2025-09-03 11:49:19 +0900

    [FE] 인증번호 입력, 다중 ref 바인딩

--- frontend/src/components/auth/SignUpForm.jsx
+++ frontend/src/components/auth/SignUpForm.jsx
@@ -2,15 +2,27 @@ import styles from './SignUpForm.module.scss';
 import EmailInput from "./EmailInput.jsx";
 import {useState} from "react";
 import VerificationInput from "./VerificationInput.jsx";
+import ProgressBar from "../common/ProgressBar.jsx";
 
 const SignUpForm = () => {
 
     // 현재 어떤 스텝인지 확인할 상태변수가 필요
-    const[step, setStep] = useState(1);
+    const[step, setStep] = useState(2);
+
+    // 프로그레스 바 노출 여부 상태관리 변수
+    const[isNext, setIsNext] =useState(false);
 
     // 이메일 중복확인이 끝났을 때 호출될 함수
     const emailSuccessHandler = () => {
-        setStep(2)
+
+        setIsNext(true); // 프로그레스 바 노출
+
+        setTimeout(() => {
+
+            setStep(2);
+            setIsNext(false);
+
+        }, 1000);
     }
 
     return (
@@ -18,6 +30,8 @@ const SignUpForm = () => {
             <div className={styles.formStepActive}>
                 {step === 1 && <EmailInput onSuccess={emailSuccessHandler}/>}
                 {step === 2 && <VerificationInput />}
+
+                {isNext && <ProgressBar/>}
             </div>
         </div>
     );
