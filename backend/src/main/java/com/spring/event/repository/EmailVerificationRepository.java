ab936ccedf8a195f351f2f5afe3ebd8f26fd38f0
dohee <doheeeq@gmail.com>
2025-09-03 12:23:51 +0900

    [BE] 인증코드 검증 API 생성

--- backend/src/main/java/com/spring/event/repository/EmailVerificationRepository.java
+++ backend/src/main/java/com/spring/event/repository/EmailVerificationRepository.java
@@ -1,7 +1,13 @@
 package com.spring.event.repository;
 
 import com.spring.event.domain.entity.EmailVerification;
+import com.spring.event.domain.entity.EventUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EmailVerificationRepository extends JpaRepository<EmailVerification, Long> {
+
+    // 사용자의 id를 통해 인증코드정보 조회
+    Optional<EmailVerification> findByEventUser(EventUser eventUser);
 }
--- backend/src/main/java/com/spring/event/repository/EventUserRepository.java
+++ backend/src/main/java/com/spring/event/repository/EventUserRepository.java
@@ -3,7 +3,10 @@ package com.spring.event.repository;
 import com.spring.event.domain.entity.EventUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EventUserRepository extends JpaRepository<EventUser, Long > {
 
     boolean existsByEmail(String email);
+    Optional<EventUser> findByEmail(String email);
 }
